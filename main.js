cc519aaa4eec5d60859b862a38e1be43f77df890
Kingerjay <Ogilikingsley96@gmail.com>
2024-12-09 16:28:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const navEL = document.querySelector(".main-nav")
+
+
+const mainNavEL = document.querySelector(".main-nav ul")
+
+const rightnavEL = document.querySelector(".right-nav")
+
+const hamburgerEl = document.querySelector(".hamburger")
+
+hamburgerEl.addEventListener("click", () => {
+    navEL.classList.toggle("nav--open");
+    rightnavEL.classList.toggle("nav-open");
+    hamburgerEl.classList.toggle("hamburger--open")
+});
+
+mainNavEL.addEventListener("click", () => {
+    navEL.classList.remove("nav--open");
+    rightnavEL.classList.remove("nav-open");
+    hamburgerEl.classList.remove("hamburger--open")
+});
+
+rightnavEL.addEventListener("click", () => {
+    navEL.classList.remove("nav--open");
+    rightnavEL.classList.remove("nav-open");
+    hamburgerEl.classList.remove("hamburger--open")
+});
